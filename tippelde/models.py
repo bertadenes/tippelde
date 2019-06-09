a9022b16e61c65f2683834d2becd3396b7d121c4
bertadenes <mr.denes.berta@gmail.com>
2019-06-09 11:08:01 +0100

    upcoming and past games

--- tippelde/models.py
+++ tippelde/models.py
@@ -12,10 +12,17 @@ class Game(models.Model):
         return "{0:s}-{1:s}".format(self.home_team, self.away_team)
 
 
+class Bookmaker(models.Manager):
+    def create_Bet(self, user_id, game_id, value):
+        bet = self.create()
+        return bet
+
+
 class Bet(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     value = models.SmallIntegerField(default=0)
+    objects = Bookmaker()
 
     def __str__(self):
         return "{0:s} {1:d}".format(self.game.__str__(), self.value)
--- tippelde/views.py
+++ tippelde/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
 from tippelde.models import Game, Bet
-# from django.http import HttpResponse
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -10,8 +11,10 @@ def index(request):
 
 
 def games(request):
-    games = Game.objects.order_by('-kickoff')
-    context = {'games': games}
+    now = timezone.now()
+    upcoming = Game.objects.order_by('kickoff').filter(kickoff__gte=now)
+    results = Game.objects.order_by('-kickoff').exclude(kickoff__gte=now)
+    context = {'results': results, 'upcoming': upcoming}
     return render(request, 'games.html', context)
 
 
@@ -19,3 +22,7 @@ def guesses(request):
     guesses = Bet.objects.filter(user=request.user)
     context = {'guesses': guesses}
     return render(request, 'guesses.html', context)
+
+
+def detail(request, game_id):
+    return HttpResponse("You're looking at question {:s}.".format(game_id))
