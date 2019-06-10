29297df7e1cba075c9405f31165f6bd60a867352
bertadenes <mr.denes.berta@gmail.com>
2019-06-10 18:07:10 +0100

    setup 0.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sweepstake',
+    version='0.1',
+    packages=['tippelde', 'tippelde.migrations', 'tippelde.templatetags', 'sweepstake'],
+    url='',
+    license='',
+    author='bertadenes',
+    author_email='mr.denes.berta@gmail.com',
+    description='Customized sweepstake project'
+)
