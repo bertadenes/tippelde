3b3b4bb8a5b8946c28a82fce782053dabf8c1fa7
bertadenes <mr.denes.berta@gmail.com>
2019-07-15 22:30:27 +0100

    game and bet model upgraded to store results

--- tippelde/models.py
+++ tippelde/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from tippelde.exceptions import EvaluatedException
+from tippelde.extras import ev
 
 
 # Create your models here.
@@ -71,19 +72,32 @@ class Game(Question):
     home_team = models.CharField(max_length=200)
     away_team = models.CharField(max_length=200)
     kickoff = models.DateTimeField()
-    outcomes = [(0, "Draw"), (1, "Home"), (2, "Away")]
-    result = models.SmallIntegerField(blank=True, null=True, choices=outcomes)
+    # obsolete with storing the actual score
+    # outcomes = [(0, "Draw"), (1, "Home"), (2, "Away")]
+    # result = models.SmallIntegerField(blank=True, null=True, choices=outcomes)
+    home_goals = models.SmallIntegerField(blank=True, null=True)
+    away_goals = models.SmallIntegerField(blank=True, null=True)
     objects = Game_Manager()
 
     def __str__(self):
         return "{0:s}-{1:s}".format(self.home_team, self.away_team)
 
     def get_result(self):
-        outcomes = ["Draw", "Home", "Away"]
-        try:
-            return "{0:s}".format(outcomes[self.result])
-        except TypeError:
+        if self.home_goals is not None and self.away_goals is not None:
+            if self.home_goals == self.away_goals:
+                return "Draw"
+            elif self.home_goals > self.away_goals:
+                return "Home"
+            elif self.home_goals < self.away_goals:
+                return "Away"
+        else:
             return "Not registered"
+        # obsolete with storing the actual score
+        # outcomes = ["Draw", "Home", "Away"]
+        # try:
+        #     return "{0:s}".format(outcomes[self.result])
+        # except TypeError:
+        #     return "Not registered"
 
     def evaluate(self):
         if self.tournament is None:
@@ -93,23 +107,32 @@ class Game(Question):
             raise EvaluatedException("This game has already been evaluated.")
         bets = Bet.objects.filter(game=self)
         for bet in bets:
-            if bet.value == self.result:
-                Score.objects.filter(user=bet.user, tournament=self.tournament).update(score=models.F('score')+self.award)
+            # obsolete with storing the actual score
+            # if bet.value == self.result:
+            self.award = ev(self.home_goals, self.away_goals, bet.home_guess, bet.away_guess)
+            Score.objects.filter(user=bet.user, tournament=self.tournament).update(score=models.F('score')+self.award)
         Game.objects.filter(id=self.id).update(evaluated=True)
         return
 
 
 class Bet(models.Model):
-    outcomes = [(0, "Draw"), (1, "Home"), (2, "Away")]
+    home_guess = models.SmallIntegerField()
+    away_guess = models.SmallIntegerField()
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    value = models.SmallIntegerField(default=0, choices=outcomes, verbose_name='outcome')
+    # obsolete with storing the actual score
+    # outcomes = [(0, "Draw"), (1, "Home"), (2, "Away")]
+    # value = models.SmallIntegerField(default=0, choices=outcomes, verbose_name='outcome')
+
     objects = Bookmaker()
 
     def __str__(self):
-        outcomes = ["Draw", "Home", "Away"]
-        return "{0:s} {1:s}".format(self.game.__str__(), outcomes[self.value])
+        return "{0:s} {1:d}-{2:d}".format(self.game.__str__(), self.home_guess, self.away_guess)
 
     def get_outcome(self):
-        outcomes = ["Draw", "Home", "Away"]
-        return "{0:s}".format(outcomes[self.value])
+        if self.home_guess == self.away_guess:
+            return "Draw"
+        elif self.home_guess > self.away_guess:
+            return "Home"
+        elif self.home_guess < self.away_guess:
+            return "Away"
